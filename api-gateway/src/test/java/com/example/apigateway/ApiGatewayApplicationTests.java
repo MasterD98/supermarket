06bc3befc255fbd42c2656d7e8c6b86d98b5b80e
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-11-08 12:15:25 +0530

    api gateway done

--- api-gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Market;
+package com.example.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketApplicationTests {
+class ApiGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
