39a15e569c6ec1810376153ac8af84969489fd15
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-11-08 12:32:12 +0530

    discovery server done

--- discovery-server/src/test/java/com/example/Market/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/example/Market/DiscoveryServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Market;
+package com.example.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketApplicationTests {
+class DiscoveryServerApplicationTests {
 
 	@Test
 	void contextLoads() {
