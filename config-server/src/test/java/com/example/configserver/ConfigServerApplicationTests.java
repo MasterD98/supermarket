2ec952c8f892066a47850f17b9c0d66459552215
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-11-08 12:22:43 +0530

    api gateway done

--- config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Market;
+package com.example.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MarketApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
