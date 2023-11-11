3a1a65b52d77fb09b40ec85e3fba6dbe0bf2629e
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2023-11-11 05:44:17 +0530

    order service done with errors 4

--- order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/orderservice/OrderServiceApplication.java
@@ -1,7 +1,5 @@
-package com.example.inventoryservice;
+package com.example.orderservice;
 
-import com.example.inventoryservice.entity.Item;
-import com.example.inventoryservice.repository.ItemRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,25 +12,4 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 
 	}
-
-	// add defaults items to inventory_service_db
-	@Bean
-	CommandLineRunner run(ItemRepository itemRepository) {
-		return args -> {
-
-			if (itemRepository.existsById(1))
-				return;
-
-			itemRepository
-					.save(new Order(1, "tikiri mari", "Biscuits", "https://example.com/oreo.jpg", 60, 7.4, "Biscuits"));
-			itemRepository.save(
-					new Order(5, "cheese-bits", "Biscuits", "https://example.com/shortbread.jpg", 35, 3.49,
-							"Biscuits"));
-			itemRepository.save(
-					new Order(10, "cream cracker", "Biscuits", "https://example.com/ginger.jpg", 45, 2.99, "Biscuits"));
-
-		};
-
-	}
-
 }
